4d185011775bc58441badb4d03b4edda0a0c5cf6
satwider47 <satwinder47@outlook.com>
2022-05-10 19:16:34 -0400

    main changes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,5 +1,6 @@
 'use strict'
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 var path = require('path');
 const {notes} = require('../../db/db.json');
 const fs = require('fs');
@@ -14,21 +15,31 @@ router.get('/notes', (req, res) => {
   });
 // post request starts here
 router.post('/notes', (req, res) => {
-    let results = notes;
-    req.body.id = results.length.toString();
+    // let results = notes;
+    const rawdata = fs.readFileSync(
+      path.join(__dirname, '../../db/db.json')
+    );
+    let results = JSON.parse(rawdata);
+    // req.body.id = results.notes.length.toString();
+    req.body.id = uuidv4();
     if (!validateNote(req.body)) {
         res.status(400).send('The entered value are empty please enter something');
       } else {
-        const note = createNewNote(req.body,results );
+        const note = createNewNote(req.body,results.notes );
         res.json(note);
       }
    
   });
   // delete request starts here
 router.delete('/notes/:id', (req, res) => {
-    let notesArray = notes;
+    // let notesArray = notes;
+    const rawdata = fs.readFileSync(
+      path.join(__dirname, '../../db/db.json') 
+    );
+    let notesArray = JSON.parse(rawdata);
+    console.log(notesArray)
     const id = req.params.id;
-    deleteNote(id,notesArray);
+    deleteNote(id,notesArray.notes);
     res.json("notes has been deleted");
    
   });
